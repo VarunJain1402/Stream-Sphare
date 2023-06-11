96c40e0fc5c05dbc41e4165d306e4094ccf05ef3
VarunJain1402 <varunjain1402@gmail.com>
2023-06-11 11:11:49 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+import { Navbar } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
